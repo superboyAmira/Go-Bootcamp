ca28d5928c94a5c7f9a9d4cb2c9b541b850909b3
Superboy <zakharenkolink@gmail.com>
2024-08-12 15:42:04 +0300

    service add

--- webService/src/internal/services/post_service.go
+++ webService/src/internal/services/post_service.go
@@ -1,7 +1,36 @@
 package services
 
-import "day06/internal/repositories"
+import (
+	"day06/internal/models"
+	"day06/internal/repositories"
+	"log/slog"
+)
 
 type PostService struct {
-	reposiry *repositories.Repository
+	reposiry repositories.Repository
+}
+
+func NewPostService(repo *repositories.PostRepository) *PostService {
+	return &PostService{
+		reposiry: repo,
+	}
+}
+
+func (r *PostService) CreteObject(model *models.Post, log *slog.Logger) (*string, error) {
+	uuid, err := r.reposiry.Create(model, log)
+	return uuid, err
+}
+func (r *PostService) GetObject(uuid string, log *slog.Logger) (any, error) {
+	model, err := r.reposiry.Get(uuid, log)
+	return model, err
+}
+
+func (r *PostService) UpdateObject(model *models.Post, log *slog.Logger) error {
+	err := r.reposiry.Update(model, log)
+	return err
+}
+
+func (r *PostService) DeleteObject(uuid string, log *slog.Logger) error {
+	err := r.reposiry.Delete(uuid, log)
+	return err
 }
